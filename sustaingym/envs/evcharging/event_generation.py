850474a309b848f7227dae009f081a8db9daa761
victor_li <52933377+liv20@users.noreply.github.com>
2022-06-21 10:02:45 -0700

    refactor method for retreiving traces from ACNData

--- sustaingym/envs/evcharging/event_generation.py
+++ sustaingym/envs/evcharging/event_generation.py
@@ -6,6 +6,7 @@ from __future__ import annotations
 from collections.abc import Set, Generator
 
 from datetime import datetime, timedelta
+import pandas as pd
 import pytz
 
 from acnportal.acndata.data_client import DataClient
@@ -65,6 +66,71 @@ def get_sessions(start_date: datetime,
         return sessions
 
 
+def get_real_events(start_date: datetime, end_date: datetime,
+                    site: str) -> pd.DataFrame:
+    """
+    Returns a pandas DataFrame of charging events.
+
+    Arguments:
+    start_date (datetime): beginning time of interval
+    end_date (datetime): ending time of interval
+    site (str): 'caltech' or 'jpl'
+
+    Returns:
+    (pd.DataFrame): DataFrame containing necessary charging info
+
+    Assumes:
+    sessions are in Pacific time
+    """
+    sessions = get_sessions(start_date, end_date, site=site, return_count=False)
+
+    arrivals = []
+    departures = []
+    requested_energies = []
+    station_ids = []
+    session_ids = []
+    estimated_departures = []
+    claimed_sessions = []
+
+    for session in sessions:
+        userInputs = session['userInputs']
+
+        connection = session['connectionTime']
+        disconnect = session['disconnectTime']
+
+        if userInputs:
+            requested_energy = userInputs[0]['kWhRequested']
+            est_depart_time = userInputs[0]['requestedDeparture']
+            est_depart_dt = parse_http_date(est_depart_time,
+                                            pytz.timezone('US/Pacific'))
+            claimed = True
+        else:
+            requested_energy = session['kWhDelivered']
+            est_depart_dt = disconnect
+            claimed = False
+
+        station_id = session['spaceID']
+        session_id = session['sessionID']
+
+        arrivals.append(connection)
+        departures.append(disconnect)
+        requested_energies.append(requested_energy)
+        station_ids.append(station_id)
+        session_ids.append(session_id)
+        estimated_departures.append(est_depart_dt)
+        claimed_sessions.append(claimed)
+    
+    return pd.DataFrame({
+        "arrival": arrivals,
+        "departure": departures,
+        "requested_energy (kWh)": requested_energies,
+        "station_id": station_ids,
+        "session_id": session_ids,
+        "estimated_departure": estimated_departures,
+        "claimed": claimed_sessions
+    })
+
+
 def get_real_event_queue(day: datetime, period: int, recompute_freq: int,
                          station_ids: Set[str], site: str,
                          use_unclaimed: bool=False) -> EventQueue:
@@ -89,19 +155,16 @@ def get_real_event_queue(day: datetime, period: int, recompute_freq: int,
     Assumes:
     sessions are in Pacific time
     """
-    sessions = get_sessions(day, day+timedelta(days=1.0), site=site, return_count=False)
+    events_df = get_real_events(day, day + timedelta(days=1), site=site)
+    if not use_unclaimed:
+        events_df = events_df[events_df['claimed']]
 
     events = []
 
-    for session in sessions:
-        userInputs = session['userInputs']
-        if not use_unclaimed and (
-            not userInputs or not type(userInputs) == list or len(userInputs) == 0):
-            continue
-
-        connection_timestamp = datetime_to_timestamp(session['connectionTime'],
+    for row, session in events_df.iterrows():
+        connection_timestamp = datetime_to_timestamp(session['arrival'],
                                                      period)
-        disconnect_timestamp = datetime_to_timestamp(session['disconnectTime'],
+        disconnect_timestamp = datetime_to_timestamp(session['departure'],
                                                      period)
         # Discard sessions with disconnect - connection <= 0, which occurs when
         # EV stays overnight
@@ -109,22 +172,13 @@ def get_real_event_queue(day: datetime, period: int, recompute_freq: int,
             continue
 
         # Discard sessions with station id's not in dataset 
-        if session['spaceID'] not in station_ids:
+        if session['station_id'] not in station_ids:
             continue
-
-        if use_unclaimed and not userInputs:
-            est_depart_timestamp = disconnect_timestamp
-            requested_energy = session['kWhDelivered']
-        else:
-            est_depart_time = userInputs[0]['requestedDeparture']
-            est_depart_dt = parse_http_date(est_depart_time,
-                                            pytz.timezone('US/Pacific'))
-            est_depart_timestamp = datetime_to_timestamp(
-                                    est_depart_dt,
-                                    period)
-            requested_energy = userInputs[0]['kWhRequested']
+        
+        est_depart_timestamp = datetime_to_timestamp(session['estimated_departure'],
+                                                     period)
+        requested_energy = session['requested_energy (kWh)']
             
-
         battery = Battery(capacity=100,
                           init_charge=100-requested_energy,
                           max_power=100)
@@ -133,8 +187,8 @@ def get_real_event_queue(day: datetime, period: int, recompute_freq: int,
             arrival=connection_timestamp,
             departure=disconnect_timestamp,
             requested_energy=requested_energy,
-            station_id=session['spaceID'],
-            session_id=session['sessionID'],
+            station_id=session['station_id'],
+            session_id=session['session_id'],
             battery=battery,
             estimated_departure=est_depart_timestamp
         )
