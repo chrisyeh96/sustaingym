0591b31027a5610d1da7b8af971ab0280a27ecd1
victor_li <52933377+liv20@users.noreply.github.com>
2022-06-28 11:36:14 -0700

    add get info to observations and rewards

--- sustaingym/envs/evcharging/observations.py
+++ sustaingym/envs/evcharging/observations.py
@@ -35,7 +35,7 @@ def get_observation_space(num_constraints: int, num_stations: int) -> spaces.Dic
     })
 
 
-def get_observation(interface: Interface, num_stations: int, evse_name_to_idx: dict, timestep: int) -> dict:
+def get_observation(interface: Interface, num_stations: int, evse_name_to_idx: dict, timestep: int, get_info: bool = False) -> dict:
     """
     Return dictionary of observations.
 
@@ -44,22 +44,29 @@ def get_observation(interface: Interface, num_stations: int, evse_name_to_idx: d
         num_stations (int) - number of stations in the garage
         evse_name_to_idx (dict) - dictionary defining the index of each EVSE
         timestep (int) - timestep of gym simulation
+        get_info (bool) - whether extra information should be returned as well
 
     Returns:
         (dict): observations from internal simulator to be used in gym
     """
     arrivals = np.zeros(shape=(num_stations,), dtype=np.int32)
-    departures = np.zeros(shape=(num_stations,), dtype=np.int32)
+    est_departures = np.zeros(shape=(num_stations,), dtype=np.int32)
     demands = np.zeros(shape=(num_stations,), dtype=np.float32)
     phases = np.zeros(shape=(num_stations,), dtype=np.float32)
+
+    if get_info:
+        actual_departures = np.zeros(shape=(num_stations,), dtype=np.int32)
 
     for session_info in interface.active_sessions():
         station_id = session_info.station_id
         station_idx = evse_name_to_idx[station_id]
 
         arrivals[station_idx] = session_info.arrival
-        departures[station_idx] = session_info.departure
+        est_departures[station_idx] = session_info.estimated_departure
         demands[station_idx] = interface.remaining_amp_periods(session_info)
+
+        if get_info:
+            actual_departures[station_idx] = session_info.departure
 
     for station_id, idx in evse_name_to_idx.items():
         phases[idx] = interface.evse_phase(station_id)
@@ -67,12 +74,30 @@ def get_observation(interface: Interface, num_stations: int, evse_name_to_idx: d
     constraint_matrix = interface.get_constraints().constraint_matrix
     magnitudes = interface.get_constraints().magnitudes
 
-    return {
+    arrivals = np.where(arrivals, arrivals - timestep, arrivals)
+    est_departures = np.where(est_departures, est_departures - timestep, est_departures)
+
+    obs = {
         "arrivals": arrivals,
-        "departures": departures,
+        "est_departures": est_departures,
         "constraint_matrix": constraint_matrix,
         "magnitudes": magnitudes,
         "demands": demands,
         "phases": phases,
         "timestep": timestep,
     }
+
+    simulator = interface._simulator
+
+    if get_info:
+        actual_departures = np.where(actual_departures, actual_departures - timestep, actual_departures)
+        info = {
+            "charging_rates": simulator.charging_rates_as_df(),
+            "active_evs": simulator.get_active_evs(),
+            "pilot_signals": simulator.pilot_signals_as_df(),
+            "active_sessions": interface.active_sessions(),
+            "departures": actual_departures,
+        }
+        return obs, info
+    else:
+        return obs
--- sustaingym/envs/evcharging/rewards.py
+++ sustaingym/envs/evcharging/rewards.py
@@ -7,11 +7,12 @@ from acnportal.acnsim.simulator import Simulator
 import numpy as np
 
 
-CHARGE_WEIGHT = 3
+CHARGE_COST_WEIGHT = 1
+DELIVERED_CHARGE_WEIGHT = 3
 CONSTRAINT_VIOLATION_WEIGHT = 10
 
 
-def get_rewards(simulator: Simulator, schedule: dict, prev_timestamp: int, timestamp: int, next_timestamp: int) -> float:
+def get_rewards(simulator: Simulator, schedule: dict, prev_timestamp: int, timestamp: int, next_timestamp: int, get_info: bool = True) -> float:
     """
     Get reward from charge received by EVs in previous timestep minus costs of
     violation constraints and amount of charge delivered in current timestep.
@@ -24,30 +25,51 @@ def get_rewards(simulator: Simulator, schedule: dict, prev_timestamp: int, times
         compute reward from charge received by EVs
     timestamp (int) - timestamp of current action taken
     next_timestamp (int) - timestamp of next action to be taken
+    get_info (bool) - return information about how reward is calculated
 
     Returns:
     total_reward (float) - total reward awarded to current timestep
     """
+    timestamp_diff = next_timestamp - timestamp
+
     schedule = schedule_to_numpy(schedule)
     # Find charging cost based on amount of charge delivered
-    charging_cost = -sum(schedule) * (next_timestamp - timestamp)
+    charging_cost = -sum(schedule) * timestamp_diff * CHARGE_COST_WEIGHT
 
     # Get charging reward based on charging rates for previous interval
-    charging_reward = (CHARGE_WEIGHT + 1) * np.sum(simulator.charging_rates[:, prev_timestamp:timestamp])
+    charging_reward = DELIVERED_CHARGE_WEIGHT * np.sum(simulator.charging_rates[:, prev_timestamp:timestamp])
 
     # Find negative reward for current violation by finding sum of current
     # going over the constraints
     current_sum = np.real(simulator.network.constraint_current(schedule, linear=False))
     magnitudes = simulator.network.magnitudes
     over_current = np.maximum(current_sum - magnitudes, 0)
-    constraint_punishment = -CONSTRAINT_VIOLATION_WEIGHT * sum(over_current) * (next_timestamp - timestamp)
+    constraint_punishment = -CONSTRAINT_VIOLATION_WEIGHT * sum(over_current) * timestamp_diff
 
     # Get total reward
     total_reward = charging_reward + charging_cost + constraint_punishment
+
+    if get_info:
+        info = {
+            "schedule": schedule,
+            "charging_cost": charging_cost,
+            "current_sum": current_sum,
+            "magnitudes": magnitudes,
+            "constraint_violation": over_current,
+            "constraint_punishment": constraint_punishment,
+            "charge_cost_weight": CHARGE_COST_WEIGHT,
+            "delivered_charge_weight": DELIVERED_CHARGE_WEIGHT,
+            "constraint_violation_weight": CONSTRAINT_VIOLATION_WEIGHT,
+            "unnormalized_total_reward": total_reward,
+        }
+
     # normalize by number of charging stations
     total_reward /= len(simulator.network.station_ids)
 
-    return total_reward
+    if get_info:
+        return total_reward, info
+    else:
+        return total_reward
 
 
 def schedule_to_numpy(schedule: dict) -> np.ndarray:
