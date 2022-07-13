33be77f77ab8fa0b0399cc622c925c6659459949
Ubuntu <ubuntu@ip-172-31-40-103.us-east-2.compute.internal>
2022-07-13 01:19:35 +0000

    wrotee constructors

--- sustaingym/envs/MO_single_agent_battery_storage_env.py
+++ sustaingym/envs/MO_single_agent_battery_storage_env.py
@@ -24,7 +24,7 @@ class MarketOperator():
         """
         self.env = env
     
-    
+
         
 
 class BatteryStorageInGridEnv(gym.Env):
@@ -41,3 +41,153 @@ class BatteryStorageInGridEnv(gym.Env):
         Current Electricity Price ($/MWh)   -Inf                    Inf
         Time (fraction of day)               0                       1
     """
+
+    # Charge efficiency
+    CHARGE_EFFICIENCY = 0.4
+    # Discharge efficiency
+    DISCHARGE_EFFICIENCY = 0.6
+    # Time step duration (hr)
+    TIME_STEP_DURATION = 5
+    # Each trajectories is one day (1440 minutes)
+    MAX_STEPS_PER_EPISODE = 288
+    # probability for running price average in state/observation space
+    eta = 0.5
+
+    def __init__(self, render_mode: Optional[str] = None, num_gens: int | None = None,
+        gen_max_production: np.ndarray | None = None, gen_costs: np.ndarray | None
+         = None, num_batteries: int | None = None, battery_max_capacity: np.ndarray | None
+         = None, battery_min_capacity: np.ndarray | None = None,
+         battery_max_discharge: np.ndarray | None = None,
+         battery_max_charge: np.ndarray | None = None,
+         battery_charge_costs: np.ndarray | None = None,
+         battery_discharge_costs: np.ndarray | None = None,
+         agent_battery_max_capacity: float | None = None,
+         agent_battery_min_capacity: float | None = None,
+         agent_battery_max_discharge: float | None = None,
+         agent_battery_max_charge: float | None = None,
+         agent_battery_max_cost: float | None = None):
+        """
+        Constructs instance of BatteryStorageInGridEnv class.
+
+        Args:
+            num_gens: number of generators
+            gen_max_production: maximum production of each generator (MW)
+            gen_costs: costs of each generator ($/MWh)
+            num_batteries: number of batteries
+            battery_max_capacity: maximum capacity of each battery (MWh)
+            battery_min_capacity: minimum capacity of each battery (MWh)
+            battery_max_discharge: maximum discharge rate of each battery (MW)
+            battery_max_charge: maximum charge rate of each battery (MW)
+            battery_charge_costs: cost of charging for each battery ($/MWh)
+            battery_discharge_costs: cost of discharging for each battery ($/MWh)
+            agent_battery_max_capacity: maximum capacity of agent's battery (MWh)
+            agent_battery_min_capacity: minimum capacity of agent's battery (MWh)
+            agent_battery_max_discharge: maximum discharge rate of agent's battery (MW)
+            agent_battery_max_charge: maximum charge rate of agent's battery (MW)
+            agent_battery_max_cost: maximum cost for charging or discharging
+            for agent's battery ($/MWh)
+        """
+        if num_gens == None:
+            self.num_gens = 10
+        else:
+            self.num_gens = num_gens
+
+        if gen_max_production == None:
+            self.gen_max_production = np.random.uniform(5, 10, size=(self.num_gens,))
+        else:
+            assert len(gen_max_production) == self.num_gens
+            self.gen_max_production = gen_max_production
+        
+        if gen_costs == None:
+            self.gen_costs = np.random.uniform(0, 5, size=(self.num_gens,))
+        else:
+            assert len(gen_costs) == self.num_gens
+            self.gen_costs = gen_costs
+        
+        if num_batteries == None:
+            self.num_batteries = 4
+        else:
+            self.num_batteries = num_batteries
+        
+        if battery_max_capacity == None:
+            self.battery_max_capacity = np.random.uniform(30, 50, size=(self.num_batteries,))
+        else:
+            assert len(battery_max_capacity) == self.num_batteries
+            self.battery_max_capacity = battery_max_capacity
+        
+        if battery_min_capacity == None:
+            self.battery_min_capacity = np.array([0] * self.num_batteries)
+        else:
+            assert len(battery_min_capacity) == self.num_batteries
+            self.battery_min_capacity = battery_min_capacity
+        
+        if battery_max_discharge == None:
+            self.battery_max_discharge = np.random.uniform(2, 4, size=(self.num_batteries,))
+        else:
+            assert len(battery_max_discharge) == self.num_batteries
+            self.battery_max_discharge = battery_max_discharge
+        
+        if battery_max_discharge == None:
+            self.battery_max_discharge = np.random.uniform(2, 4, size=(self.num_batteries,))
+        else:
+            assert len(battery_max_discharge) == self.num_batteries
+            self.battery_max_discharge = battery_max_discharge
+
+        if battery_max_charge == None:
+            self.battery_max_charge = np.random.uniform(-3, -1, size=(self.num_batteries,))
+        else:
+            assert len(battery_max_charge) == self.num_batteries
+            self.battery_max_charge = battery_max_charge
+        
+        if battery_charge_costs == None:
+            self.battery_charge_costs = np.random.uniform(0, 5, size=(self.num_batteries,))
+        else:
+            assert len(battery_charge_costs) == self.num_batteries
+            self.battery_charge_costs = battery_charge_costs
+        
+        if battery_discharge_costs == None:
+            self.battery_discharge_costs = np.random.uniform(0, 4, size=(self.num_batteries,))
+        else:
+            assert len(battery_discharge_costs) == self.num_batteries
+            self.battery_charge_costs = battery_discharge_costs
+        
+        self.battery_init_charge = np.zeros((self.num_batteries, ))
+
+        for i in range(self.num_batteries):
+            self.battery_init_charge[i] = (self.battery_min_capacity[i] +
+                                            self.battery_max_capacity[i]) / 2.0
+        
+        if agent_battery_max_capacity == None:
+            self.agent_battery_max_capacity = np.random.uniform(30, 50)
+        else:
+            self.agent_battery_max_capacity = agent_battery_max_capacity
+        
+        if agent_battery_min_capacity == None:
+            self.agent_battery_min_capacity = 0.0
+        else:
+            self.agent_battery_min_capacity = agent_battery_min_capacity
+        
+        if agent_battery_max_discharge == None:
+            self.agent_battery_max_discharge = np.random.uniform(2, 4)
+        else:
+            self.agent_battery_max_discharge = agent_battery_max_discharge
+        
+        if agent_battery_max_charge == None:
+            self.agent_battery_max_charge = np.random.uniform(-3, -1)
+        else:
+            self.agent_battery_max_charge = agent_battery_max_charge
+        
+        if agent_battery_max_cost == None:
+            self.agent_battery_max_cost = np.random.uniform(10, 20)
+        else:
+            self.agent_battery_max_cost = agent_battery_max_cost
+        
+        # action space is two values for the charging and discharging costs
+        self.action_space = spaces.Box(low=0,
+                                       high=self.agent_battery_max_cost, shape=(2, ),
+                                       dtype=np.float32)
+        # observation space is current energy level and current price
+        self.observation_space = spaces.Box(
+            -np.inf, np.inf, shape=(3, ), dtype=np.float32
+        )
+        self.init = False
