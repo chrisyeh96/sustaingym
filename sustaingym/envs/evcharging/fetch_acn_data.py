15b07fd3ff719458e87bff2ddd34bf5ea7e88b14
liv20 <vhli2020@gmail.com>
2022-08-03 16:50:47 +0000

    add script for saving data from ACN-Data

---
+++ sustaingym/envs/evcharging/fetch_acn_data.py
@@ -0,0 +1,20 @@
+from sustaingym.envs.evcharging.utils import *
+from datetime import datetime, timedelta
+import os
+import pandas as pd
+
+print(DEFAULT_DATE_RANGES)
+print(DATE_FORMAT)
+
+for start, end in DEFAULT_DATE_RANGES:
+    for site in ['caltech', 'jpl']:
+        start_dt = datetime.strptime(start, DATE_FORMAT)
+        end_dt = datetime.strptime(end, DATE_FORMAT)
+        df = fetch_real_events(start_dt, end_dt, site=site)
+
+        fdir = os.path.join('sustaingym', 'data', 'acn_evcharging_data', site)
+        os.makedirs(fdir, exist_ok=True)
+        fname = f'{start} {end}.csv'
+        fpath = os.path.join(fdir, fname)
+
+        df.to_csv(fpath, compression='gzip', index=False)   
