9980e97865f52ccbfbc2b9e06b0610015b0ef9ea
liv20 <vhli2020@gmail.com>
2022-11-18 09:01:56 +0000

    full sending algorithms

--- sustaingym/algorithms/evcharging/base_algorithm.py
+++ sustaingym/algorithms/evcharging/base_algorithm.py
@@ -1,86 +1,102 @@
+"""
+This module implements baseline algorithms for the EVChargingEnv.
+"""
 from __future__ import annotations
 
 from collections.abc import Sequence
 from typing import Any
 
 import cvxpy as cp
+from gym import spaces
 import numpy as np
 from stable_baselines3.common.on_policy_algorithm import OnPolicyAlgorithm
 from tqdm import tqdm
 
 from sustaingym.envs.evcharging.ev_charging import EVChargingEnv
+from sustaingym.envs.evcharging.utils import solve_optimization_problem
 
 
 class BaseOnlineAlgorithm:
-    """Abstract class for online algorithms for the evcharging gym.
+    """Abstract class for online scheduling algorithms for EVChargingEnv.
 
     Subclasses are expected to implement the get_action() method.
-
-    Attributes:
-        name: name of the algorithm
     """
-    def get_action(self, observation: dict[str, Any], env: EVChargingEnv) -> np.ndarray:
-        """Returns an action based on EV charging observations.
+    # Continuous maximum action
+    C_MAX_ACTION = 1
+
+    # Discrete maximum action
+    D_MAX_ACTION = 4
+
+    def __init__(self, env: EVChargingEnv):
+        """
+        Args:
+            env: EV charging environment
+        """
+        self.env = env
+        self.continuous_action_space = isinstance(self.env.action_space, spaces.Box)
+    
+    def _get_max_action(self) -> int:
+        return self.C_MAX_ACTION if self.continuous_action_space else self.D_MAX_ACTION
+
+    def get_action(self, observation: dict[str, Any]) -> np.ndarray:
+        """Returns an action based on gym observations.
 
         Args:
             observation: information concerning the current state of charge
                 in the EV Charging gym.
-            env: EV charging environment
 
         Returns:
             (np.ndarray) next action
         """
         raise NotImplementedError
 
-    def run(self, seeds: Sequence[int] | int, env: EVChargingEnv) -> tuple[list[float], dict]:
-        """
+    def run(self, seeds: Sequence[int] | int) -> dict[str, list[float]]:
+        """Runs the scheduling algorithm and returns the resulting rewards.
+
         Runs the scheduling algorithm for the current period and returns the
         resulting reward.
 
         Args:
             seeds: list of randoms seeds to set environment to a particular day
-                or an integer for the number of times to evaluate
-            env: EV charging environment
+                or an integer for the number of times to evaluate.
 
         Returns:
-            list of total rewards for each episode
+            total_rewards: list of total rewards for each episode
             dict of list of each individual reward component
         """
-        total_rewards = []
-        reward_components = {'profit': [], 'carbon_cost': [], 'excess_charge': []}
         if isinstance(seeds, int):
             seeds = [i for i in range(seeds)]
+
+        reward_breakdown = {
+            'reward': [], 'profit': [], 'carbon_cost': [], 'excess_charge': []
+        }
+
+        print("Seeds: ", seeds)
         for seed in tqdm(seeds):
-            done = False
-            obs = env.reset(seed=seed)
+            # Reset environment
+            obs = self.env.reset(seed=seed)
             episode_reward = 0.0
-            episode_breakdown = {'profit': 0.0, 'carbon_cost': 0.0, 'excess_charge': 0.0}
+            
+            print(repr(self.env))
+
+            # Run episode until finished
+            done = False
+
             while not done:
-                action = self.get_action(obs, env)  # type: ignore
-                obs, reward, done, info = env.step(action)
-                for comp in info['reward']:
-                    episode_breakdown[comp] += info['reward'][comp]
+                action = self.get_action(obs)
+                obs, reward, done, info = self.env.step(action)
                 episode_reward += reward
-            total_rewards.append(episode_reward)
-            for comp in episode_breakdown:
-                reward_components[comp].append(episode_breakdown[comp])
-        return total_rewards, reward_components
+
+            for rb in info['reward_breakdown']:
+                reward_breakdown[rb] += info['reward_breakdown'][rb]
+            reward_breakdown['reward'].append(episode_reward)
+
+        return reward_breakdown
 
 
 class GreedyAlgorithm(BaseOnlineAlgorithm):
-    """Greedily charges at each time step.
-
-    Attributes:
-        project_action: whether environment's action projection should be used
-    """
-    def __init__(self, project_action: bool = False):
-        """
-        Args:
-            project_action: whether environment's action projection should be used
-        """
-        self.project_action = project_action
-
-    def get_action(self, observation: dict[str, Any], env: EVChargingEnv) -> np.ndarray:
+    """Class for greedy charging at each time step."""
+    def get_action(self, observation: dict[str, Any]) -> np.ndarray:
         """Returns greedy charging action.
 
         Args:
@@ -89,9 +105,25 @@ class GreedyAlgorithm(BaseOnlineAlgorithm):
         Returns:
             *See get_action() in BaseOnlineAlgorithm.
         """
-        action = np.full(env.num_stations, 4)
-        if self.project_action:
-            return env.project_action(action)
+        # Send full charging rate wherever demands are non-zero
+        return np.where(observation['demands'] > 0, self._get_max_action(), 0)
+
+
+class RandomAlgorithm(BaseOnlineAlgorithm):
+    """Class for random charging at each time step."""
+    def get_action(self, observation: dict[str, Any]) -> np.ndarray:
+        """Returns random charging action.
+
+        Args:
+            *See get_action() in BaseOnlineAlgorithm.
+
+        Returns:
+            *See get_action() in BaseOnlineAlgorithm.
+        """
+        if self.continuous_action_space:
+            action = np.random.randint(0, 5, size=self.env.num_stations)
+        else:
+            action = np.random.random(size=self.env.num_stations)
         return action
 
 
@@ -99,6 +131,7 @@ class MPC(BaseOnlineAlgorithm):
     """Model predictive control.
 
     Attributes:
+
         first_run: if True, cvxpy needs setting up
         lookahead: number of timesteps to forecast future trajectory
     """
@@ -106,87 +139,82 @@ class MPC(BaseOnlineAlgorithm):
         """
         Args:
             lookahead: number of timesteps to forecast future trajectory
+            less than environment's moer_forecast_steps
         """
-        self.first_run = True
+        super(BaseOnlineAlgorithm, self).__init__()
         self.lookahead = lookahead
 
+        # Optimization problem setup similar to action projection
+
+        # MPC action trajectory 
+        self.traj = cp.Variable((self.env.num_stations, self.lookahead), nonneg=True)
+
+        # Aggregate magnitude (A) must be less than observation magnitude (A)
+        phase_factor = np.exp(1j * np.deg2rad(self.env.cn._phase_angles))
+        A_tilde = self.env.cn.constraint_matrix * phase_factor[None, :]
+        agg_magnitude = cp.abs(A_tilde @ self.traj) * self.env.ACTION_SCALE_FACTOR
+        magnitude_limit = np.tile(np.expand_dims(self.env.cn.magnitudes, axis=1), (1, self.lookahead))
+        
+        # Parameters
+
+        # Current timestep demands in kWh
+        self.demands_cvx = cp.Parameter((self.env.num_stations,), nonneg=True)
+
+        # Forecasted moers
+        self.moers = cp.Parameter((self.lookahead,), nonneg=True)
+
+        # Boolean mask for when an EV is expected to leave
+        self.mask = cp.Parameter((self.env.num_stations, self.lookahead), nonneg=True)
+
+        # Maximize profit and minimize carbon cost subject to network constraints
+        profit = cp.sum(self.traj) * self.env.PROFIT_FACTOR
+        carbon_cost = cp.sum(self.traj @ self.moers) * self.env.CARBON_COST_FACTOR
+        objective = cp.Maximize(profit - carbon_cost)
+        constraints = [
+            # Cannot charge after EV leaves using estimation as a proxy
+            self.traj <= self.mask,
+            # Cannot overcharge demand
+            cp.sum(self.traj, axis=1) <= self.demands_cvx / self.env.A_PERS_TO_KWH / self.env.ACTION_SCALE_FACTOR,
+            # Cannot break network constraints
+            agg_magnitude <= magnitude_limit
+        ]
+
+        # Formulate problem
+        self.prob = cp.Problem(objective, constraints)
+        assert self.prob.is_dpp() and self.prob.is_dcp()
+
     def get_action(self, observation: dict[str, Any], env: EVChargingEnv) -> np.ndarray:
-        """Returns first action of the k-step optimal trajectory."""
-        if self.first_run:
-            self.traj = cp.Variable((env.num_stations, self.lookahead), nonneg=True)  # units A*periods*ACTION_SCALE_FACTOR
+        """Returns first action of the MPC trajectory."""
+        self.demands_cvx.value = observation['demands']
+        self.moers.value = observation['forecasted_moer'][:self.lookahead]
 
-            # Aggregate magnitude (ACTION_SCALING_FACTOR*A) must be less than observation magnitude (A)
-            phase_factor = np.exp(1j * np.deg2rad(env.cn._phase_angles))
-            A_tilde = env.cn.constraint_matrix * phase_factor[None, :]
-            agg_magnitude = cp.abs(A_tilde @ self.traj) * env.ACTION_SCALE_FACTOR  # convert to A
-            magnitude_limit = np.tile(np.expand_dims(env.cn.magnitudes, axis=1), (1, self.lookahead))
-
-            self.demands_cvx = cp.Parameter(env.num_stations, nonneg=True)
-            self.moers = cp.Parameter(self.lookahead, nonneg=True)
-            self.mask = cp.Parameter((env.num_stations, self.lookahead), nonneg=True)
-
-            profit = cp.sum(self.traj) * env.PROFIT_FACTOR  # units $*ACTION_SCALE_FACTOR
-            carbon_cost = cp.sum(self.traj @ self.moers) * env.CARBON_COST_FACTOR  # units $*ACTION_SCALE_FACTOR
-            objective = cp.Maximize(profit - carbon_cost)  # units $*ACTION_SCALE_FACTOR
-
-            constraints = [
-                self.traj <= self.mask,
-                cp.sum(self.traj, axis=1) <= self.demands_cvx / env.A_PERS_TO_KWH / env.ACTION_SCALE_FACTOR,
-                agg_magnitude <= magnitude_limit
-            ]
-            self.prob = cp.Problem(objective, constraints)
-
-            assert self.prob.is_dpp() and self.prob.is_dcp()
-            self.first_run = False
-
-        self.demands_cvx.value = env.raw_observation(observation, 'demands')  # units A*periods
-
-        self.moers.value = env.raw_observation(observation, 'forecasted_moer')[:self.lookahead]  # kg CO2 per kWh
-
-        cur_est_dep = np.maximum(1, np.round(env.max_timestep * observation['est_departures'])).astype(np.int32)
-        # if estimated departure has already passed, assume car will stay for 1 timestep
+        # If estimated departure has already passed, assume car will stay for 1 timestep
+        cur_est_dep = np.maximum(1., observation['est_departures']).astype(np.int32)
         cur_est_dep = np.where(observation['demands'] > 0, cur_est_dep, 0)
 
         mask = np.zeros((env.num_stations, self.lookahead))
         for i in range(env.num_stations):
-            mask[i, :cur_est_dep[i]] = env.MAX_ACTION + env.EPS
+            mask[i, :cur_est_dep[i]] = self._get_max_action()
         self.mask.value = mask
 
-        try:
-            self.prob.solve(warm_start=True, solver=cp.MOSEK)
-        except cp.SolverError:
-            # print('Default MOSEK solver failed in MPC. Trying ECOS.')
-            self.prob.solve(solver=cp.ECOS)
-            if self.prob.status != 'optimal':
-                print(f'prob.status = {self.prob.status}')
-            if 'infeasible' in self.prob.status:
-                # your problem should never be infeasible. So now go debug
-                import pdb
-                pdb.set_trace()
-        action = self.traj.value[:, 0]  # take first action
-
-        if env.action_type == 'discrete':
-            # round to nearest integer if above threshold
-            action = np.where(np.modf(action)[0] > env.ROUND_UP_THRESH, np.round(action), np.floor(action))
-        return action
+        solve_optimization_problem(self.prob)
+        return self.traj.value[:, 0]  # take first action
 
 
 class RLAlgorithm(BaseOnlineAlgorithm):
-    """
-    Uses output of RL algorithm as action.
+    """Uses output of RL algorithm as action.
 
     Attributes:
         rl_model: stable baselines RL model
         project_action: whether environment's action projection should be used
     """
-    def __init__(self, rl_model: OnPolicyAlgorithm, project_action: bool):
+    def __init__(self, rl_model: OnPolicyAlgorithm):
         """
         Args:
             rl_model: stable baselines RL model
             name: algorithm identifier
         """
         self.rl_model = rl_model
-        self.project_action = project_action
 
     def get_action(self, observation: dict[str, Any], env: EVChargingEnv) -> np.ndarray:
         """Returns output of RL model.
@@ -202,31 +230,3 @@ class RLAlgorithm(BaseOnlineAlgorithm):
             return env.project_action(action)
         return action
 
-
-class RandomAlgorithm(BaseOnlineAlgorithm):
-    """
-    Uses random output as action.
-
-    Attributes:
-        project_action: whether environment's action projection should be used
-    """
-    def __init__(self, project_action: bool = False):
-        """
-        Args:
-            project_action: whether environment's action projection should be used
-        """
-        self.project_action = project_action
-
-    def get_action(self, observation: dict[str, Any], env: EVChargingEnv) -> np.ndarray:
-        """Returns output of RL model.
-
-        Args:
-            *See get_action() in BaseOnlineAlgorithm.
-
-        Returns:
-            *See get_action() in BaseOnlineAlgorithm.
-        """
-        action = np.random.randint(0, 5, size=env.num_stations)
-        if self.project_action:
-            return env.project_action(action)
-        return action
