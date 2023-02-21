49b9e0151d1e0b0ade135b144e10ffba76c619b3
liv20 <vhli2020@gmail.com>
2023-02-21 07:40:29 +0000

    add time delay to multiagent

--- sustaingym/envs/evcharging/ev_charging_multiagent.py
+++ sustaingym/envs/evcharging/ev_charging_multiagent.py
@@ -3,7 +3,7 @@ The module implements a multi-agent version of the EVChargingEnv.
 """
 from __future__ import annotations
 
-import functools
+from collections import deque
 from typing import Any
 
 import gymnasium
@@ -23,6 +23,7 @@ class MultiAgentEVChargingEnv(Env):
     - observation_spaces
     """
     def __init__(self, data_generator: AbstractTraceGenerator,
+                 periods_delay: int = 0,
                  moer_forecast_steps: int = 36,
                  project_action_in_env: bool = True,
                  verbose: int = 0):
@@ -38,11 +39,14 @@ class MultiAgentEVChargingEnv(Env):
         self.possible_agents = self.agents
         self.max_num_agents = self.num_agents
 
+        self.periods_delay = periods_delay
+        self.past_obs: deque = deque([], maxlen=self.periods_delay)
+
         self.observation_spaces = {agent: spaces.Dict({
-            'est_departures':  spaces.Box(-288, 288, shape=(1,), dtype=np.float32),
+            'est_departures':  spaces.Box(-288, 288, shape=(self.max_num_agents,), dtype=np.float32),
             'demands':         spaces.Box(0,
                                         self.single_env.data_generator.requested_energy_cap,
-                                        shape=(1,), dtype=np.float32),
+                                        shape=(self.max_num_agents,), dtype=np.float32),
             'prev_moer':       spaces.Box(0, 1.0, shape=(1,), dtype=np.float32),
             'forecasted_moer': spaces.Box(0, 1.0, shape=(self.single_env.moer_forecast_steps,), dtype=np.float32),
             'timestep':        spaces.Box(0, 1.0, shape=(1,), dtype=np.float32),
@@ -53,20 +57,37 @@ class MultiAgentEVChargingEnv(Env):
             for agent in self.agents
         }
 
-    def _create_dict_from_obs_agg(self, obs_agg: dict[str, Any]) -> dict[str, dict[str, Any]]:
+    def _create_dict_from_obs_agg(self, obs_agg: dict[str, Any], init: bool = True) -> dict[str, dict[str, Any]]:
         """Spread observation across agents."""
         obs = {}
         for i, agent in enumerate(self.agents):
-            ob = {
-                'est_departures': obs_agg['est_departures'][i:i+1],
-                'demands': obs_agg['demands'][i:i+1],
-                'prev_moer': obs_agg['prev_moer'],
-                'forecasted_moer': obs_agg['forecasted_moer'],
-                'timestep': obs_agg['timestep'],
-            }
-            obs[agent] = ob
-        self.state = obs
-        return obs
+            # ob = {
+            #     'est_departures': obs_agg['est_departures'],
+            #     'demands': obs_agg['demands'],
+            #     'prev_moer': obs_agg['prev_moer'],
+            #     'forecasted_moer': obs_agg['forecasted_moer'],
+            #     'timestep': obs_agg['timestep'],
+            # }
+            obs[agent] = obs_agg
+        
+        if self.periods_delay == 0:
+            return obs
+
+        if init:  # initialize past_obs by repeating first observation
+            self.past_obs.clear()
+            for _ in range(self.periods_delay):
+                self.past_obs.append(obs.copy())
+            return obs
+        else:
+            first_obs = self.past_obs.popleft()
+            self.past_obs.append(obs)
+
+            td_obs = obs.copy()  # time-delay observation
+            for i, agent in enumerate(self.agents):
+                for var in ['est_departures', 'demands']:
+                    td_obs[agent][var] = first_obs[agent][var]
+                    td_obs[agent][var][i] = obs[agent][var][i]
+            return td_obs
  
     def _create_dict_from_infos_agg(self, infos_agg: dict[str, Any]) -> dict[str, dict[str, Any]]:
         """Each agent gets same global info."""
