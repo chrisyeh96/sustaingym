bfd4d13a5ea61f17adab88fe3e2111d73d746141
Christopher Yeh <chrisyeh96@users.noreply.github.com>
2023-06-03 19:05:00 -0700

    Fix data generation seed

--- sustaingym/envs/evcharging/event_generation.py
+++ sustaingym/envs/evcharging/event_generation.py
@@ -8,7 +8,7 @@ or from sampling from an artificial data model, respectively.
 """
 from __future__ import annotations
 
-from datetime import datetime, timedelta
+from datetime import timedelta
 import uuid
 
 import acnportal.acnsim as acns
@@ -18,9 +18,9 @@ import sklearn.mixture as mixture
 
 from .train_gmm_model import create_gmms
 from .utils import (COUNT_KEY, DATE_FORMAT, DEFAULT_PERIOD_TO_RANGE, GMM_KEY,
-                    MINS_IN_DAY, REQ_ENERGY_SCALE, STATION_USAGE_KEY, AM_LA,
-                    DefaultPeriodStr, SiteStr, load_gmm_model,
-                    site_str_to_site, get_real_events)
+                    MINS_IN_DAY, REQ_ENERGY_SCALE, STATION_USAGE_KEY,
+                    DefaultPeriodStr, SiteStr, get_real_events, load_gmm_model,
+                    site_str_to_site, to_la_dt)
 from sustaingym.data.load_moer import MOERLoader
 
 
@@ -68,7 +68,7 @@ class AbstractTraceGenerator:
                 custom date period. If custom, the input must be a 2-tuple
                 of strings with both strings in the format YYYY-MM-DD.
                 Otherwise, should be a default period string.
-            requested_energy_cap: maximum amount of requested energy allowed (kWh)
+            requested_energy_cap: max amount of requested energy allowed (kWh)
             seed: seed for random sampling
         """
         # Name of site, name of stations on site, and number of stations on site
@@ -83,8 +83,7 @@ class AbstractTraceGenerator:
             self.date_range_str = date_period
 
         # Convert strings to datetime objects
-        self.date_range = tuple(datetime.strptime(x, DATE_FORMAT).replace(tzinfo=AM_LA)
-                                for x in self.date_range_str)
+        self.date_range = tuple(to_la_dt(s) for s in self.date_range_str)
 
         # Number of days in date range used for sekecting random day
         self.num_days_in_date_range = (self.date_range[1] - self.date_range[0]).days + 1
@@ -124,7 +123,7 @@ class AbstractTraceGenerator:
         self.rng = np.random.default_rng(seed=seed)
 
     def _create_events(self) -> pd.DataFrame:
-        """Creates a DataFrame of charging events for a single day.
+        """Creates a DataFrame of charging events for the current day.
 
         Returns:
             DataFrame containing charging info.
@@ -146,7 +145,7 @@ class AbstractTraceGenerator:
         raise NotImplementedError
 
     def get_event_queue(self) -> tuple[acns.EventQueue, list[acns.EV], int]:
-        """Creates an EventQueue from a DataFrame of charging information.
+        """Creates an EventQueue for the current day, and then updates the day.
 
         Sessions are added as Plugin events, and Recompute events are added
         every period so that the algorithm can be continually called. Unplug
@@ -164,7 +163,8 @@ class AbstractTraceGenerator:
 
         for i in range(len(samples)):
             # Cap maximum requested energy
-            requested_energy = min(samples['requested_energy (kWh)'].iloc[i], self.requested_energy_cap)
+            requested_energy = min(samples['requested_energy (kWh)'].iloc[i],
+                                   self.requested_energy_cap)
 
             # Create battery with initial charge at a minimum zero
             battery = acns.Linear2StageBattery(
@@ -199,6 +199,8 @@ class AbstractTraceGenerator:
                 event = acns.RecomputeEvent(timestep)
                 events.append(event)
         events = acns.EventQueue(events)
+
+        self._update_day()
         return events, evs, num_plugin
 
     def get_moer(self) -> np.ndarray:
@@ -210,7 +212,7 @@ class AbstractTraceGenerator:
                 five-min time steps. Units kg CO2 per kWh. Rows are sorted
                 chronologically.
         """
-        return self.moer_loader.retrieve(self.day.replace(tzinfo=AM_LA))
+        return self.moer_loader.retrieve(self.day)
 
 
 class RealTraceGenerator(AbstractTraceGenerator):
@@ -242,15 +244,18 @@ class RealTraceGenerator(AbstractTraceGenerator):
                 True, the generator uses the energy delivered in the session and
                 the disconnect time in place of those attributes, eliminating
                 real-world uncertainty in user requests.
+            seed: if sequential, the seed determines which day to start on
             *See AbstractTraceGenerator for more arguments
         """
         super().__init__(site, date_period, requested_energy_cap, seed)
 
         self.sequential = sequential
-        if sequential:  # seed day before first update
-            self.day = self.date_range[0] - timedelta(days=1)
+        if sequential:
+            if seed is None:
+                seed = 0
+            self.set_seed(seed)  # set day based on seed
         else:
-            self._update_day()
+            self._update_day()  # pick a random day
 
         self.use_unclaimed = use_unclaimed
 
@@ -264,22 +269,11 @@ class RealTraceGenerator(AbstractTraceGenerator):
                 f'Current day: {self.day.strftime(DATE_FORMAT)}')
 
     def set_seed(self, seed: int | None) -> None:
-        """If days are sequential, sets the day. Otherwise, sets the random number generator."""
-        # TODO(chris): issue when using sequential. The typical usage is
-        # generator.set_seed(seed=1)
-        # event_queue, evs, num_plug_events = generator.get_event_queue()
-        #   -> which calls generator._create_events()
-        #        -> which calls generator._update_day(). This increments the
-        #           day from the seed we set earlier!
-        #
-        # So the following sequence of calls would lead to MOER values offset
-        # from the event_queue by 1 day:
-        # generator.set_seed(seed=1)
-        # moer = generator.get_moer()
-        # event_queue, evs, num_plug_events = generator.get_event_queue()
+        """If days are sequential, sets the day. Otherwise, seeds the random
+        number generator.
+        """
         if self.sequential:
-            if seed is not None:
-                self.day = self.date_range[0] + timedelta(days=seed % self.num_days_in_date_range)
+            self.day = self.date_range[0] + timedelta(days=seed % self.num_days_in_date_range)
         else:
             super().set_seed(seed)
 
@@ -290,7 +284,7 @@ class RealTraceGenerator(AbstractTraceGenerator):
             if self.day > self.date_range[1]:  # cycle back when day has exceeded date range
                 self.day = self.date_range[0]
         else:
-            super()._update_day()
+            super()._update_day()  # pick a random day
 
     def _create_events(self) -> pd.DataFrame:
         """Retrieves and filters real events from a given day.
@@ -299,7 +293,6 @@ class RealTraceGenerator(AbstractTraceGenerator):
             DataFrame of real sessions with datetimes in terms of timestamps.
             *See _create_events() in AbstractTraceGenerator for more information.
         """
-        self._update_day()
         df = self.events_df[(self.day <= self.events_df.arrival) &
                             (self.events_df.arrival < self.day + timedelta(days=1))]
         if not self.use_unclaimed:
@@ -327,7 +320,6 @@ class RealTraceGenerator(AbstractTraceGenerator):
 
         # remove sessions with estimated departure before connection
         df = df[df['estimated_departure'] > df['arrival']]
-
         return df.copy()
 
 
@@ -392,6 +384,9 @@ class GMMsTraceGenerator(AbstractTraceGenerator):
         self.gmm: mixture.GaussianMixture = data[GMM_KEY]
         self.cnt: np.ndarray = data[COUNT_KEY]
         self.station_usage: np.ndarray = data[STATION_USAGE_KEY]
+
+        self.set_seed(seed)
+        self._update_day()
 
     def __repr__(self) -> str:
         """Returns string representation of GMMsTraceGenerator."""
@@ -465,7 +460,6 @@ class GMMsTraceGenerator(AbstractTraceGenerator):
         Returns:
             DataFrame of artificial sessions.
         """
-        self._update_day()
         # number of events from empirical pdf
         n = int(self.rng.choice(self.cnt))
         samples = self._sample(n)
