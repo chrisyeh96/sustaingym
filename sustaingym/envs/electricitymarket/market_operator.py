e0bd9be5f250ddf6eb4fd36f741f3ab9a9709ebd
Christopher Yeh <chrisyeh96@users.noreply.github.com>
2023-10-12 08:44:59 +0000

    Update market_operator.py and networks/base.py

--- sustaingym/envs/electricitymarket/market_operator.py
+++ sustaingym/envs/electricitymarket/market_operator.py
@@ -24,7 +24,7 @@ class MarketOperator:
             network: Network, defines network properties
             congestion: bool, whether to include congestion constraints
             time_step_duration: float, length of a time step in hours
-            h: int, horizon (settlement interval) in # of time steps
+            h: int, horizon (settlement interval) in # of time steps, >= 0
             milp: bool, whether to solve a mixed-integer linear program to
                 enforce that charge * discharge = 0
         """
@@ -41,19 +41,20 @@ class MarketOperator:
 
         # Variables
         self.soc = cp.Variable((N_B, h + 2), name='soc')  # state of charge (MWh)
-        self.out = cp.Variable((N_D + N_G + 2*N_B, h + 1), name='dispatch')  # dispatch (MWh)
-        demand_dis = self.out[:N_D]  # demands (loads, MWh)
-        gen_dis = self.out[N_D: N_D+N_G]  # generator dispatch (MWh)
-        bat_c = self.out[N_D+N_G: N_D+N_G+N_B]  # battery charge (MWh)
-        bat_d = self.out[-N_B:]  # battery discharge (MWh)
+        self.dispatch = cp.Variable((N_D + N_G + 2*N_B, h + 1), name='dispatch')  # dispatch (MW)
+        demand_dis = self.dispatch[:N_D]  # demands (loads, MW)
+        gen_dis = self.dispatch[N_D: N_D+N_G]  # generator dispatch (MW)
+        bat_c = self.dispatch[N_D+N_G: N_D+N_G+N_B]  # battery charge (MW)
+        bat_d = self.dispatch[-N_B:]  # battery discharge (MW)
 
         # Parameters
-        # - bat_c_max and bat_d_max are constants provided by the network, but
-        #     we make them parameters so that we can more easily "disable" the
-        #     batteries
+        # bat_c_max and bat_d_max are constants provided by the network, but
+        # we need to make them parameters with a time dimension so that they
+        # can be modified by the MILP to enforce that the battery is either
+        # charging or discharging, not both.
         self.demand = cp.Parameter((N_D, h + 1), name='demand forecast')  # (MW)
-        self.bat_c_max = cp.Parameter(N_B, name='battery max charge')  # (MW)
-        self.bat_d_max = cp.Parameter(N_B, name='battery max discharge')  # (MW)
+        self.bat_c_max = cp.Parameter((N_B, h + 1), name='battery max charge')  # (MW)
+        self.bat_d_max = cp.Parameter((N_B, h + 1), name='battery max discharge')  # (MW)
         self.c_bat_c = cp.Parameter((N_B, h + 1), name='battery cost charge')  # charge cost ($/MWh)
         self.c_bat_d = cp.Parameter((N_B, h + 1), name='battery cost discharge')  # discharge cost ($/MWh)
         self.soc_init = cp.Parameter(N_B, nonneg=True, name='soc initial')  # initial SOC (MWh)
@@ -62,17 +63,17 @@ class MarketOperator:
         # Constraints
         constraints = [
             # demand is fixed
-            demand_dis == self.demand * time_step_duration,
+            demand_dis == self.demand,
 
             # generation limits
-            gen_dis >= net.g_min[:, None] * time_step_duration,
-            gen_dis <= net.g_max[:, None] * time_step_duration,
+            gen_dis >= net.g_min[:, None],
+            gen_dis <= net.g_max[:, None],
 
             # battery limits
             bat_c >= 0,
-            bat_c <= self.bat_c_max[:, None] * time_step_duration,
+            bat_c <= self.bat_c_max,
             bat_d >= 0,
-            bat_d <= self.bat_d_max[:, None] * time_step_duration,
+            bat_d <= self.bat_d_max,
 
             # battery range
             self.soc >= net.soc_min,
@@ -89,21 +90,23 @@ class MarketOperator:
         ]
 
         # power balance
-        self.power_balance_constr: cp.Constraint = cp.sum(net.J @ self.out, axis=0) == 0
+        self.power_balance_constr: cp.Constraint = (
+            cp.sum(net.J @ self.dispatch * time_step_duration, axis=0) == 0
+        )
         constraints.append(self.power_balance_constr)
 
         if self.congestion:
             # power flow and line flow limits (aka congestion constraints)
-            Hp = net.H @ net.J @ self.out
+            Hp = net.H @ net.J @ self.dispatch  # (in MW)
             self.congestion_constrs = [
-                Hp <= net.f_max[:, None] * time_step_duration,
-                Hp >= -net.f_max[:, None] * time_step_duration,
+                Hp <= net.f_max[:, None],
+                Hp >= -net.f_max[:, None],
             ]
             constraints.extend(self.congestion_constrs)
 
         # Objective function
         obj = 0
-        for tau in range(h+1):
+        for tau in range(h + 1):
             # add up all generator costs
             # - net.c_gen[:, 0] is a constant, so we can ignore it in the objective
             obj += (
@@ -130,13 +133,13 @@ class MarketOperator:
 
     def get_dispatch(self, demand: np.ndarray, demand_forecast: np.ndarray,
                      c_bat: np.ndarray | None, soc: np.ndarray | None,
-                     soc_final: np.ndarray | None, verbose: bool = True
+                     soc_final: np.ndarray | None, verbose: bool = False
                      ) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
         """Determines dispatch values.
 
         Args:
-            demand: array, shape [N_D], demands (MW)
-            demand_forecast: array, shape [N_D, h], forecasted demands (MW)
+            demand: array, shape [N_D], nodal demands (MW)
+            demand_forecast: array, shape [N_D, h], nodal demands forecast (MW)
             c_bat: array or None, shape [2, N_B, h + 1],
                 c_bat[0] is battery charge bid price ($/MWh),
                 c_bat[1] is battery discharge cost ($/MWh),
@@ -149,10 +152,10 @@ class MarketOperator:
             verbose: bool, whether to print detailed outputs
 
         Returns:
-            gen_dis: array, shape [N_G], generator dispatch values
-            bat_d: array, shape [N_B], battery discharge amounts
-            bat_c: array, shape [N_B], battery charge amounts
-            price: array, shape [N]
+            gen_dis: array, shape [N_G], generator dispatch (MW)
+            bat_d: array, shape [N_B], battery discharge amounts (MWh)
+            bat_c: array, shape [N_B], battery charge amounts (MWh)
+            prices: array, shape [N], nodal prices ($/MWh)
         """
         net = self.network
         N = net.N      # num buses
@@ -162,15 +165,14 @@ class MarketOperator:
         h = self.h     # horizon
 
         # shape [N_D, h+1], units MW
-        self.demand.value = np.empty([N_D, h + 1])
-        self.demand.value[:, 0] = demand
-        self.demand.value[:, 1:] = demand_forecast
+        self.demand.value = np.concatenate(
+            [demand[:, None], demand_forecast], axis=1)
 
         if c_bat is None:
             assert soc is None
             assert soc_final is None
-            self.bat_c_max.value = np.zeros(N_B)
-            self.bat_d_max.value = np.zeros(N_B)
+            self.bat_c_max.value = np.zeros([N_B, h + 1])
+            self.bat_d_max.value = np.zeros([N_B, h + 1])
             self.soc_init.value = np.zeros(N_B)
             self.soc_final.value = np.zeros(N_B)
             self.c_bat_c.value = np.zeros([N_B, h + 1])
@@ -178,8 +180,8 @@ class MarketOperator:
         else:
             assert soc is not None
             assert soc_final is not None
-            self.bat_c_max.value = net.bat_c_max
-            self.bat_d_max.value = net.bat_d_max
+            self.bat_c_max.value = np.tile(net.bat_c_max, (1, h+1))
+            self.bat_d_max.value = np.tile(net.bat_d_max, (1, h+1))
             self.soc_init.value = soc
             self.soc_final.value = soc_final
             # c_bat has shape [2, N_B, h + 1]
@@ -231,29 +233,43 @@ class MarketOperator:
         else:
             prices = lam * np.ones(N)
 
-        gen_dis = self.out.value[N_D:N_D+N_G]  # generator
-        bat_c = self.out.value[N_D+N_G: N_D+N_G+N_B]  # charge
-        bat_d = self.out.value[-N_B:]  # discharge
+        dispatch = self.dispatch.value
+        gen_dis = dispatch[N_D:N_D+N_G]  # generator dispatch (MW)
+        bat_c = dispatch[N_D+N_G: N_D+N_G+N_B] * self.time_step_duration  # charge (MWh)
+        bat_d = dispatch[-N_B:] * self.time_step_duration  # discharge (MWh)
 
         if verbose:
-            Δsoc = net.eff_c * bat_c - (1. / net.eff_d) * bat_d
+            Δsoc = self.soc.value[:, 1:] - self.soc[:, :-1]  # shape [N_B, h+1]
 
             with np.printoptions(precision=2):
                 print(f'========== dispatch output ==========')
                 print(f'network: {N} buses, {net.M} lines, {N_D} loads, '
                       f'{N_G} generators, {N_B} batteries')
                 print(f'SCED horizon: {h}')
-                print(f'total load: {self.demand.value.sum(axis=0)}')
-                print(f'objective value: {self.prob.value}')
-                print(f'battery soc initial: {self.soc_init.value}, final target: ≥{self.soc_final.value}')
-                print(f'battery soc: {self.soc.value}')
-                print(f'battery Δsoc: {Δsoc}')
-                print(f'battery discharge x: {bat_d}')
-                print(f'battery charge x: {bat_c}')
-                print(f'battery charge costs: {self.c_bat_c.value}')
-                print(f'battery discharge costs: {self.c_bat_d.value}')
-                print(f'generator dispatch: {gen_dis}')
-                print(f'generator costs: {net.c_gen[:, 1]}')
+                print(f'total load (MW): {self.demand.value.sum(axis=0)}')
+                print(f'objective value ($): {self.prob.value}')
+                print(f'battery soc initial (MWh): {self.soc_init.value}, final target: ≥{self.soc_final.value}')
+                print(f'battery soc (MWh): {self.soc.value}')
+                print(f'battery Δsoc (MWh): {Δsoc}')
+                print(f'battery discharge (MWh): {bat_d}')
+                print(f'battery charge (MWh): {bat_c}')
+                print(f'battery charge costs ($/Mwh): {self.c_bat_c.value}')
+                print(f'battery discharge costs ($/Mwh): {self.c_bat_d.value}')
+                print(f'generator dispatch (MW): {gen_dis}')
+                print(f'generator costs ($/MWh): {net.c_gen[:, 1]}')
+
+                congested = False
+                if self.congestion:
+                    congested_days = (
+                        (self.congestion_constrs[0].dual_value > 1e-5).any(axis=0)
+                        | (self.congestion_constrs[1].dual_value > 1e-5).any(axis=0))
+                    print(f'congested: {congested_days}')
+                    congested = congested_days.any()
+                if congested:
+                    print(f'nodal prices ($/MWh): {prices}')
+                else:
+                    print(f'uniform price ($/MWh): {lam}')
+
                 print()
 
         return gen_dis[:, 0], bat_d[:, 0], bat_c[:, 0], prices
