313ef5b689ca7d5cd9bfef7523c3bfe935f9331f
Ethan Wilk <ethanwilk@gmail.com>
2024-05-30 14:26:06 -0700

    Removing unnecessary files

--- sustaingym/__init__.py
+++ sustaingym/__init__.py
@@ -2,28 +2,15 @@ from gymnasium.envs.registration import register
 
 register(
     id='sustaingym/EVCharging-v0',
-    entry_point='sustaingym.envs.evcharging:EVChargingEnv',
-    nondeterministic=False
-)
-
-# register(
-#     id='sustaingym/ElectricityMarket-v0',
-#     entry_point='sustaingym.envs.electricitymarket:ElectricityMarketEnv'
-# )
-
-# register(
-#     id='sustaingym/DataCenter-v0',
-#     entry_point='sustaingym.envs:DataCenterEnv'
-# )
-
-register(
-    id='sustaingym/Building-v0',
-    entry_point='sustaingym.envs.building:BuildingEnv',
-    nondeterministic=False
+    entry_point='sustaingym.envs:EVChargingEnv'
 )
 
 register(
-    id='sustaingym/Cogen-v0',
-    entry_point='sustaingym.envs.cogen:CogenEnv',
-    nondeterministic=False
+    id='sustaingym/ElectricityMarket-v0',
+    entry_point='sustaingym.envs:ElectricityMarketEnv'
+)
+
+register(
+    id='sustaingym/DataCenter-v0',
+    entry_point='sustaingym.envs:DataCenterEnv'
 )
